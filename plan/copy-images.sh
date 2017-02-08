81db027a4e948d21d8bb3d382de273067261b773
Romilly Cocking <romilly.cocking@gmail.com>
2017-02-08 21:09:50 +0000

    Updated map, added images

---
+++ plan/copy-images.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/bash
+cp resources/images/microbit/2-betterUX-large-preview-opt.jpg plan/manuscript/images/
+cp ../manuscript/images/hw.png plan/manuscript/images/
--- plan/workshop-content.mm
+++ plan/workshop-content.mm
@@ -1,6 +1,6 @@
-<map version="freeplane 1.3.0">
+<map version="freeplane 1.5.9">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node ID="ID_1505706922" CREATED="1486207335557" MODIFIED="1486455135234" LINK="Pi-plus-microbit-intro-workshop.mm"><richcontent TYPE="NODE">
+<node FOLDED="false" ID="ID_1505706922" CREATED="1486207335557" MODIFIED="1486455135234" LINK="Pi-plus-microbit-intro-workshop.mm"><richcontent TYPE="NODE">
 
 <html>
   <head>
@@ -21,25 +21,29 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
 <hook NAME="MapStyle">
+    <properties fit_to_viewport="false;" show_note_icons="true"/>
 
 <map_styles>
-<stylenode LOCALIZED_TEXT="styles.root_node">
-<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
-<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="as_parent">
 <font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.note"/>
 <stylenode LOCALIZED_TEXT="defaultstyle.floating">
 <edge STYLE="hide_edge"/>
 <cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
 </stylenode>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
 <font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
 </stylenode>
@@ -53,7 +57,7 @@
 <icon BUILTIN="yes"/>
 </stylenode>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
 <font SIZE="18"/>
 </stylenode>
@@ -73,12 +77,81 @@
 </stylenode>
 </map_styles>
 </hook>
-<node TEXT="Introduction" POSITION="right" ID="ID_742155583" CREATED="1486207406805" MODIFIED="1486207415011"/>
-<node TEXT="Setting Up" POSITION="right" ID="ID_1188307060" CREATED="1486207423965" MODIFIED="1486207472251"/>
-<node TEXT="Hello World!" POSITION="right" ID="ID_270340901" CREATED="1486207515253" MODIFIED="1486207530874">
-<node TEXT="Using mu" ID="ID_1582411923" CREATED="1486208235453" MODIFIED="1486208250235"/>
+<node TEXT="Introduction" POSITION="right" ID="ID_742155583" CREATED="1486207406805" MODIFIED="1486207415011">
+<node TEXT="What you will do" ID="ID_1888294199" CREATED="1486566160144" MODIFIED="1486571025720" LINK="resources/images/microbit/2-betterUX-large-preview-opt.jpg"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      In this 60 minute workshop you will learn how to connect a BBC micro:bit to a Raspberry Pi.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      You will use the <b>mu</b>&#160;editor to write MicroPython programs and run them on the micro:bit.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      The experiments will show you some of the things the micro:bit can do. Use them as a base to build your own projects.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      You will also get some links to help you explore once the workshop is over
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Setting Up" POSITION="right" ID="ID_1188307060" CREATED="1486207423965" MODIFIED="1486569784806"/>
+<node TEXT="Hello World!" POSITION="right" ID="ID_270340901" CREATED="1486207515253" MODIFIED="1486569841751" LINK="../manuscript/images/hw.png"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Open a browser on http://bit.ly/pibit
+    </p>
+  </body>
+</html>
+
+</richcontent>
 <node TEXT="Typing in a program" ID="ID_589419269" CREATED="1486208271693" MODIFIED="1486208282371">
-<node TEXT="Import statements" ID="ID_1730334690" CREATED="1486208289246" MODIFIED="1486208299706"/>
+<node TEXT="Import statements" ID="ID_1730334690" CREATED="1486208289246" MODIFIED="1486567289484"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Courier">from microbit import *</font>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Tells MicroPython that you want to use the&#160;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
 <node TEXT="display.scroll" ID="ID_238988938" CREATED="1486208302589" MODIFIED="1486211420877"/>
 </node>
 <node TEXT="Running a program" ID="ID_1077282608" CREATED="1486208253813" MODIFIED="1486208269026"/>
@@ -88,7 +161,7 @@
 <node TEXT="Fortune Cookies" POSITION="left" ID="ID_1421625296" CREATED="1486207638398" MODIFIED="1486207650778"/>
 <node TEXT="Reaction Timer" POSITION="left" ID="ID_1797844906" CREATED="1486207679934" MODIFIED="1486207693260"/>
 <node TEXT="Moving and shaking" POSITION="left" ID="ID_1743941950" CREATED="1486207720070" MODIFIED="1486208123571"/>
-<node TEXT="What&apos;s next" POSITION="left" ID="ID_1507584763" CREATED="1486207815086" MODIFIED="1486207824585">
+<node TEXT="What&apos;s next" FOLDED="true" POSITION="left" ID="ID_1507584763" CREATED="1486207815086" MODIFIED="1486207824585">
 <node TEXT="More stuff the micro:bit can do" ID="ID_699194136" CREATED="1486208144933" MODIFIED="1486208174932"/>
 <node TEXT="Where to find out more" ID="ID_1125025479" CREATED="1486208176053" MODIFIED="1486208189811"/>
 </node>
